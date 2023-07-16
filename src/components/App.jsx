16d86a94ea1c7b5d89115b16c4ad7e8377a3d6d3
Oleksandr Tympshenko <hermeusmanua@gmail.com>
2023-07-16 18:04:36 +0300

    code refactoring using react hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import React, { Component } from 'react'; 
 import { Searchbar } from './Searchbar';
 import { ImageGallery } from './ImageGallery';
 import { ImageGalleryItem } from './ImageGalleryItem';
@@ -6,105 +5,94 @@ import { Button } from './Button';
 import { getImages } from 'js/getImages';
 import { Loader } from './Loader';
 import { Modal } from './Modal';
+import { useState, useEffect } from 'react';
 
-export class App extends Component {
-  state = { 
-    input: "", 
-    images: [],
-    page: 0, 
-    isLoading: false,
-    error: null,
-    modalOpen: false,
-    imgSrc: '', 
-  }
-  
-  onSubmit = evt => { 
+export const App = () => {
+  const [input, setInput] = useState(''); 
+  const [images, setImages] = useState([]); 
+  const [page, setPage] = useState(0); 
+  const [isLoading, setIsLoading] = useState(false); 
+  const [error, setError] = useState(null); 
+  const [modalOpen, setModalOpen] = useState(false); 
+  const [imgSrc, setImgSrc] = useState(''); 
+
+  const onSubmit = (evt) => { 
     evt.preventDefault();
-    this.setState({
-      input: evt.target.firstElementChild.nextSibling.value,
-      page: 1,
-      isLoading: true,
-      images: []
-    }); 
+    setInput(evt.target.firstElementChild.nextSibling.value); 
+    setPage(1); 
+    setIsLoading(true); 
+    setImages([]); 
+    setError(null); 
   }
 
-  onLoad = () => { 
-    this.setState({ isLoading: true });
-    this.setState((state => { 
-      return { page: state.page + 1}
-    }))
-  }
-
-  onClickImg = (evt) => {
+  const onClickImg = (evt) => {
     evt.preventDefault();
-    this.setState({ modalOpen: true, imgSrc: evt.currentTarget.href });
+    setModalOpen(true); 
+    setImgSrc(evt.currentTarget.href); 
   }
 
-  onClickOverlay = (evt) => {
-    this.setState({ modalOpen: false, imgSrc: '', }); 
-  }
-
-  onKeyDown = (evt) => {
-    if (evt.key.toLowerCase() === 'escape') this.setState({ modalOpen: false, imgSrc: '', }); 
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.isLoading) {
-      const inputValue = this.state.input; 
-      const page = this.state.page;
-
-        getImages(inputValue, page)
-        .then((response) => {
-          this.setState((state) => { 
-            if (response.length === 12) {
-              return { images: [...state.images, ...response] }
-            } else {
-              return { images: [...state.images, ...response], error: "END"}
-            }
-          })
-        })
-        .catch((error) => this.setState({ error: error }))
-        .finally(this.setState({ isLoading: false }));
-     
+  const onKeyDown = (evt) => { 
+    if (evt.key.toLowerCase() === 'escape') {
+      setModalOpen(false); 
+      setImgSrc('');
     }
-  } 
-
-  render() { 
-    const { images, isLoading, error, modalOpen, imgSrc} = this.state; 
-
-    return (
-      <>
-        <Searchbar
-          onSubmit={isLoading ? null : this.onSubmit}
-        >
-        </Searchbar>
-        <ImageGallery>
-          <ImageGalleryItem
-            images={images}
-            onClick={modalOpen ? null : this.onClickImg}
-            onKeyDown={modalOpen ? this.onKeyDown : null}
-          >
-          </ImageGalleryItem>
-        </ImageGallery>
-        {(error !== null) &&
-          <p style={{ textAlign: 'center', }}>
-            { (error === "END" && images.length === 0) ? 
-              "No photo by your request" : 
-              (error === "END" ) ?
-              "These are all images found for your request" :
-              "Ooops, something get wrong..."
-            }
-          </p>
-        }
-        {isLoading && <Loader></Loader>}
-        {error === null && !isLoading && images.length > 0 &&
-          <Button onLoad={this.onLoad}></Button>
-        }
-        <Modal
-          imgHref={imgSrc}
-          modalOpen={modalOpen}
-          onClickOverlay={modalOpen ? this.onClickOverlay : null}></Modal>
-      </>
-    )
   }
-};
+
+  const onClickOverlay = () => {
+    setModalOpen(false); 
+    setImgSrc(''); 
+  }
+
+  const onLoad = () => { 
+    setIsLoading(true); 
+    setPage(page + 1)
+  }
+
+  useEffect(() => {  
+    if (isLoading) {
+      getImages(input, page).then((response) => {
+        setImages([...images, ...response]);
+        if (response.length !== 12) {
+          setError("END");
+        }
+      })
+        .catch((error) => setError(error))
+        .finally(setIsLoading(false));
+    }
+  }, [isLoading])
+
+  return (
+    <>
+      <Searchbar
+        onSubmit={isLoading ? null : onSubmit}
+      >
+      </Searchbar>
+      <ImageGallery>
+        <ImageGalleryItem
+          images={images}
+          onClick={modalOpen ? null : onClickImg}
+          onKeyDown={modalOpen ? onKeyDown : null}
+        >
+        </ImageGalleryItem>
+      </ImageGallery>
+      {(error !== null) &&
+        <p style={{ textAlign: 'center', }}>
+          { (error === "END" && images.length === 0) ?
+            "No photo by your request" :
+            (error === "END" ) ?
+            "These are all images found for your request" :
+            "Ooops, something get wrong..."
+          }
+        </p>
+      }
+      {isLoading && <Loader></Loader>}
+      {error === null && !isLoading && images.length > 0 &&
+        <Button onLoad={onLoad}></Button>
+      }
+      <Modal
+        imgHref={imgSrc}
+        modalOpen={modalOpen}
+        onClickOverlay={modalOpen ? onClickOverlay : null}></Modal>
+    </>
+  )
+}
